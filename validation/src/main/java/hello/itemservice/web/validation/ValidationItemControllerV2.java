6d5b2ef9864a28cbed5990eacd0abcae578b2342
SiruDuck <potato32122@gmail.com>
2023-09-11 15:00:29 +0900

    검증

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -83,7 +84,7 @@ public class ValidationItemControllerV2 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         //에러처리가 되면 필드의 값이 날라가기 때문에 보존시켜줘야함
         //검증 로직
@@ -114,6 +115,94 @@ public class ValidationItemControllerV2 {
             log.info("errors={}",bindingResult);
             return "validation/v2/addForm";
         }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        log.info("objectName={}",bindingResult.getObjectName());
+        log.info("target={}",bindingResult.getTarget());
+
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName())){
+            bindingResult.addError(new FieldError("item","itemName",item.getItemName(),false,new String[]{"required.item.itemName"} ,null,null));
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000){
+            bindingResult.addError(new FieldError("item","price",item.getPrice(),false, new String[]{"range.item.price"},new Object[]{1000,100000}, null));
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999){
+            bindingResult.addError(new FieldError("item","quantity",item.getQuantity(),false, new String[]{"max.item.quantity"},new Object[]{9999}, null));
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                bindingResult.addError(new ObjectError("item",new String[]{"totalPriceMin"},new Object[]{10000,resultPrice},null));
+                //object는 데이터가 넘어오는게 아님
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()){      //bindingResult가 Errors를 가지고 있으면
+            //BindingResult는 자동으로 view로 넘어감
+            log.info("errors={}",bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        log.info("objectName={}",bindingResult.getObjectName());
+        log.info("target={}",bindingResult.getTarget());
+
+        if(bindingResult.hasErrors()){
+            log.info("errors={}",bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //ValidationUtils.rejectIfEmpty(bindingResult,"itemName","required");  -> Empty , 공백 같은 단순한 기능만 제공
+        //검증 로직 위의 ValidationUtils.rejectIfEmpty 와 같은 코드임
+        if (!StringUtils.hasText(item.getItemName())){
+            bindingResult.rejectValue("itemName", "required");
+            //new String[] {"required.item.itemName","required"};
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000){
+            bindingResult.rejectValue("price","range", new Object[]{1000,1000000},null);
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999){
+            bindingResult.rejectValue("quantity","max", new Object[]{9999},null);
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                bindingResult.addError(new ObjectError("item",new String[]{"totalPriceMin"},new Object[]{10000,resultPrice},null));
+                bindingResult.reject("totalPriceMin", new Object[]{10000,resultPrice},null);
+                //object는 데이터가 넘어오는게 아님
+            }
+        }
+
+//        //검증에 실패하면 다시 입력 폼으로
+//        if(bindingResult.hasErrors()){      //bindingResult가 Errors를 가지고 있으면
+//            //BindingResult는 자동으로 view로 넘어감
+//            log.info("errors={}",bindingResult);
+//            return "validation/v2/addForm";
+//        }
 
         //성공 로직
         Item savedItem = itemRepository.save(item);
